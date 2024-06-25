9dc6aeaf38532896909bc72f8c5385a77d59045c
Hamza Siddiq <92462002+hamza-siddiq@users.noreply.github.com>
2024-06-25 13:59:10 -0700

    Revert "Optimize slideshow.js"
    This reverts commit b096f8b4dc0a8498238b7242dcc032b5e98a1ba4.

--- assets/slideshow.js
+++ assets/slideshow.js
@@ -1,249 +1,259 @@
+/**
+ *  @class
+ *  @function SlideShow
+ */
+
 if (!customElements.get('slide-show')) {
   class SlideShow extends HTMLElement {
     constructor() {
       super();
-      this.state = {
-        initialized: false,
-        currentIndex: 0,
-      };
-      this.options = {};
-      this.callbacks = {};
-    }
+      const slideshow = this;
 
-    connectedCallback() {
-      if (!this.state.initialized) {
-        this.init();
-      }
-    }
-
-    init() {
-      this.parseDataset();
-      this.setupDOMReferences();
-      this.createFlickityInstance();
-      this.setupEventListeners();
-      this.state.initialized = true;
-    }
-
-    parseDataset() {
-      const booleanAttributes = ['dots', 'fade', 'wrapAround', 'adaptiveHeight'];
-      const numberAttributes = ['autoplay'];
-
-      booleanAttributes.forEach(attr => {
-        this.options[attr] = this.dataset[attr] === 'true';
-      });
-
-      numberAttributes.forEach(attr => {
-        const value = parseInt(this.dataset[attr], 10);
-        this.options[attr] = isNaN(value) ? false : value;
-      });
-
-      this.options.align = this.dataset.align === 'center' ? 'center' : 'left';
-      this.options.rightToLeft = document.dir === 'rtl';
-    }
-
-    setupDOMReferences() {
-      this.slides = this.querySelectorAll('.carousel__slide');
-      this.prevButton = this.querySelector('.flickity-prev');
-      this.nextButton = this.querySelector('.flickity-next');
-    }
-
-    createFlickityInstance() {
-      const args = this.getFlickityArgs();
-      this.flickity = new Flickity(this, args);
-    }
-
-    getFlickityArgs() {
-      const baseArgs = {
-        cellSelector: '.carousel__slide',
-        prevNextButtons: false,
-        ...this.options,
-      };
-
-      const specificArgs = this.getSpecificArgs();
-      return { ...baseArgs, ...specificArgs };
-    }
-
-    getSpecificArgs() {
-      const classListContains = (className) => this.classList.contains(className);
-
-      if (classListContains('image-with-text-slideshow__image')) {
-        return {
-          draggable: false,
-          asNavFor: this.closest('.image-with-text-slideshow').querySelector('.image-with-text-slideshow__content')
+      let dots = slideshow.dataset.dots === 'true',
+        slideshow_slides = Array.from(slideshow.querySelectorAll('.carousel__slide')),
+        autoplay = slideshow.dataset.autoplay == 'false' ? false : parseInt(slideshow.dataset.autoplay, 10),
+        align = slideshow.dataset.align == 'center' ? 'center' : 'left',
+        fade = slideshow.dataset.fade == 'true' ? true : false,
+        prev_button = slideshow.querySelector('.flickity-prev'),
+        next_button = slideshow.querySelector('.flickity-next'),
+        custom_dots = slideshow.querySelectorAll('.flickity-custom-dots'),
+        animations = [],
+        rightToLeft = document.dir === 'rtl',
+        currentIndex = 0,
+        args = {
+          wrapAround: true,
+          cellAlign: align,
+          pageDots: dots,
+          contain: true,
+          fade: fade,
+          autoPlay: autoplay,
+          rightToLeft: rightToLeft,
+          prevNextButtons: false,
+          cellSelector: '.carousel__slide'
         };
+      if (slideshow.classList.contains('image-with-text-slideshow__image')) {
+        let main_slideshow = slideshow.parentNode.querySelector('.image-with-text-slideshow__content');
+        args.draggable = false;
+        args.asNavFor = main_slideshow;
       }
-
-      if (classListContains('collection-grid__carousel') || classListContains('main-slideshow')) {
-        return this.getAnimatedArgs();
+      if (slideshow.classList.contains('image-with-text-slideshow__content')) {
+        args.adaptiveHeight = true;
       }
-
-      if (classListContains('products')) {
-        return {
-          wrapAround: false,
-          on: { ready: () => this.centerArrows() }
-        };
-      }
-
-      return {};
-    }
-
-    getAnimatedArgs() {
-      if (document.body.classList.contains('animations-true') && typeof gsap !== 'undefined') {
-        return {
-          on: {
-            ready: () => this.animateSlides(this.state.currentIndex),
-            change: (index) => {
-              const prevIndex = this.state.currentIndex;
-              this.state.currentIndex = index;
-              this.animateSlides(index, prevIndex);
-            },
-            scroll: classListContains('collection-grid__carousel') ? this.handleCollectionGridScroll.bind(this) : null
+      if (slideshow.classList.contains('testimonials__carousel')) {
+        args.on = {
+          ready: function () {
+            window.dispatchEvent(new Event('resize'));
           }
         };
       }
-      return {};
-    }
+      if (slideshow.classList.contains('collection-grid__carousel')) {
+        if (document.body.classList.contains('animations-true') && typeof gsap !== 'undefined') {
+          args.on = {
+            scroll: function (progress) {
+              let flkSlideWidth = slideshow.querySelectorAll('.collection-card')[0].clientWidth + 25,
+                extra_window_space = slideshow.getBoundingClientRect().left;
 
-    setupEventListeners() {
-      if (this.prevButton) {
-        this.prevButton.addEventListener('click', () => this.flickity.previous());
+              this.slides.forEach((the_slide, j) => {
+
+                let scale_amount = 1,
+                  translateX_amount = 0,
+                  rotate_amount = 0,
+                  zindex = 10,
+                  opacity_amount = 1,
+                  slide = the_slide.cells[0].element,
+                  slide_offset = slide.getBoundingClientRect().left;
+
+                if (slide_offset - extra_window_space < 0) {
+
+                  scale_amount = 1 + ((slide_offset - extra_window_space) / 1200);
+                  opacity_amount = 1 + ((slide_offset - extra_window_space) / 200);
+                  translateX_amount = ((slide_offset - extra_window_space)) * -1;
+                  rotate_amount = ((slide_offset - extra_window_space) / 10) * -1;
+
+                } else {
+                  scale_amount = 1;
+                  opacity_amount = 1;
+                  translateX_amount = 0;
+                  rotate_amount = 0;
+                }
+
+                if (slide_offset + 5 - extra_window_space < 0) {
+                  zindex = 5;
+                } else {
+                  zindex = 10;
+                }
+
+                slide.style.zIndex = zindex;
+
+                gsap.set(slide.querySelector('.collection-card--inner'), {
+                  'transform': 'perspective(1000px) translateX(' + translateX_amount + 'px) rotateY(' + rotate_amount + 'deg) translateZ(0)',
+                  'opacity': opacity_amount
+                });
+
+                gsap.set(slide.querySelector('.collection-card__link'), {
+                  'transform': 'scale(' + scale_amount + ') translateZ(0)'
+                });
+
+              });
+            }
+          };
+        }
+
       }
-      if (this.nextButton) {
-        this.nextButton.addEventListener('click', () => this.flickity.next());
+      if (slideshow.classList.contains('main-slideshow')) {
+        if (slideshow.classList.contains('desktop-height-image') || slideshow.classList.contains('mobile-height-image')) {
+          args.adaptiveHeight = true;
+        }
+        if (document.body.classList.contains('animations-true') && typeof gsap !== 'undefined') {
+          slideshow.prepareAnimations(slideshow, animations);
+          args.on = {
+            ready: function () {
+              slideshow.animateSlides(0, slideshow, animations);
+            },
+            change: function (index) {
+              let previousIndex = fizzyUIUtils.modulo(this.selectedIndex - 1, this.slides.length);
+
+              slideshow.animateReverse(previousIndex, animations);
+              slideshow.animateSlides(index, slideshow, animations);
+            }
+          };
+        }
+      }
+      if (slideshow.classList.contains('products')) {
+        args.wrapAround = false;
+        args.on = {
+          ready: function () {
+            var flickity = this;
+            window.addEventListener('resize.center_arrows', function () {
+              slideshow.centerArrows(flickity, prev_button, next_button);
+            });
+            window.dispatchEvent(new Event('resize.center_arrows'));
+          }
+        };
+      }
+      const flkty = new Flickity(slideshow, args);
+
+      slideshow.dataset.initiated = true;
+
+
+      if (prev_button) {
+        prev_button.addEventListener('click', (event) => {
+          flkty.previous();
+        });
+        prev_button.addEventListener('keyup', (event) => {
+          flkty.previous();
+        });
+        next_button.addEventListener('click', (event) => {
+          flkty.next();
+        });
+        next_button.addEventListener('keyup', (event) => {
+          flkty.next();
+        });
       }
 
       if (Shopify.designMode) {
-        this.addEventListener('shopify:block:select', this.handleShopifyBlockSelect.bind(this));
-      }
-
-      window.addEventListener('resize', this.handleResize.bind(this));
-    }
-
-    handleShopifyBlockSelect(event) {
-      const index = Array.from(this.slides).indexOf(event.target);
-      if (index !== -1) {
-        this.flickity.select(index);
-      }
-    }
-
-    handleResize() {
-      if (this.classList.contains('products')) {
-        this.centerArrows();
-      }
-      this.flickity.resize();
-    }
-
-    handleCollectionGridScroll(progress) {
-      requestAnimationFrame(() => {
-        const flkSlideWidth = this.querySelector('.collection-card').clientWidth + 25;
-        const extraWindowSpace = this.getBoundingClientRect().left;
-
-        this.slides.forEach((slide, j) => {
-          const slideOffset = slide.getBoundingClientRect().left;
-          const relativeOffset = slideOffset - extraWindowSpace;
-
-          if (relativeOffset < 0) {
-            const scale = 1 + (relativeOffset / 1200);
-            const opacity = 1 + (relativeOffset / 200);
-            const translateX = -relativeOffset;
-            const rotate = -relativeOffset / 10;
-            const zIndex = relativeOffset + 5 < 0 ? 5 : 10;
-
-            gsap.set(slide.querySelector('.collection-card--inner'), {
-              transform: `perspective(1000px) translateX(${translateX}px) rotateY(${rotate}deg) translateZ(0)`,
-              opacity: opacity,
-              zIndex: zIndex
-            });
-
-            gsap.set(slide.querySelector('.collection-card__link'), {
-              transform: `scale(${scale}) translateZ(0)`
-            });
-          } else {
-            gsap.set(slide.querySelector('.collection-card--inner'), {
-              clearProps: 'all'
-            });
-            gsap.set(slide.querySelector('.collection-card__link'), {
-              clearProps: 'all'
-            });
-          }
+        slideshow.addEventListener('shopify:block:select', (event) => {
+          let index = slideshow_slides.indexOf(event.target);
+          flkty.select(index);
         });
+      }
+
+    }
+    prepareAnimations(slideshow, animations) {
+      if (!slideshow.dataset.animationsReady) {
+        document.fonts.ready.then(function () {
+          slideshow.querySelectorAll('.slideshow__slide').forEach((item, i) => {
+            let tl = gsap.timeline({
+              paused: true
+            }),
+              button_offset = 0;
+
+
+            animations[i] = tl;
+
+            tl
+              .to(item.querySelector('.slideshow__slide-content'), {
+                duration: 0,
+                autoAlpha: 1
+              });
+            if (item.querySelector('.subheading')) {
+              tl
+                .fromTo(item.querySelector('.subheading'), {
+                  opacity: 0
+                }, {
+                  duration: 0.5,
+                  opacity: 0.6
+                }, 0);
+
+              button_offset += 0.5;
+            }
+            if (item.querySelector('h1')) {
+              let h1_duration = 0.5 + ((item.querySelectorAll('h1 .line-child div').length - 1) * 0.05);
+              tl
+                .from(item.querySelectorAll('h1 .line-child div'), {
+                  duration: h1_duration,
+                  yPercent: '100',
+                  stagger: 0.05
+                }, 0);
+              button_offset += h1_duration;
+            }
+if (item.querySelector('p:not(.subheading)')) {
+  tl.from(item.querySelector('p:not(.subheading)'), {
+    duration: 0.5,
+    opacity: 0,
+    y: '20px'
+  }, 0);
+  button_offset += 0.5;
+}
+            if (item.querySelectorAll('.button')) {
+              tl
+                .fromTo(item.querySelectorAll('.button'), {
+                  opacity: 0
+                }, {
+                  duration: 0.5,
+                  opacity: 1,
+                  stagger: 0.05
+                }, '-=0.4');
+            }
+            item.dataset.timeline = tl;
+          });
+        });
+        slideshow.dataset.animationsReady = true;
+      }
+    }
+    animateSlides(i, slideshow, animations) {
+      let flkty = Flickity.data(slideshow),
+        active_slide = flkty.selectedElement;
+      document.fonts.ready.then(function () {
+        animations[i].restart();
       });
     }
-
-    animateSlides(currentIndex, previousIndex) {
-      if (previousIndex !== undefined) {
-        this.animateReverse(previousIndex);
-      }
-
-      const slide = this.slides[currentIndex];
-      const timeline = slide.timeline || this.createSlideTimeline(slide);
-
-      timeline.restart();
+    animateReverse(i, animations) {
+      animations[i].reverse();
     }
+    centerArrows(flickity, prev_button, next_button) {
+      let first_cell = flickity.cells[0],
+        max_height = 0,
 
-    animateReverse(index) {
-      const slide = this.slides[index];
-      if (slide.timeline) {
-        slide.timeline.reverse();
-      }
-    }
+        image_height = first_cell.element.querySelector('.product-featured-image').clientHeight;
 
-    createSlideTimeline(slide) {
-      const timeline = gsap.timeline({ paused: true });
-      
-      const content = slide.querySelector('.slideshow__slide-content');
-      const subheading = slide.querySelector('.subheading');
-      const heading = slide.querySelector('h1');
-      const paragraph = slide.querySelector('p:not(.subheading)');
-      const buttons = slide.querySelectorAll('.button');
+      flickity.cells.forEach((item, i) => {
+        if (item.size.height > max_height) {
+          max_height = item.size.height;
+        }
+      });
 
-      timeline.to(content, { duration: 0, autoAlpha: 1 });
+      if (max_height > image_height) {
+        let difference = (max_height - image_height) / -2;
 
-      if (subheading) {
-        timeline.fromTo(subheading, { opacity: 0 }, { duration: 0.5, opacity: 0.6 }, 0);
-      }
-
-      if (heading) {
-        const headingLines = heading.querySelectorAll('.line-child div');
-        timeline.from(headingLines, {
-          duration: 0.5 + ((headingLines.length - 1) * 0.05),
-          yPercent: 100,
-          stagger: 0.05
-        }, 0);
-      }
-
-      if (paragraph) {
-        timeline.from(paragraph, { duration: 0.5, opacity: 0, y: '20px' }, 0);
-      }
-
-      if (buttons.length) {
-        timeline.fromTo(buttons, { opacity: 0 }, { duration: 0.5, opacity: 1, stagger: 0.05 }, '-=0.4');
-      }
-
-      slide.timeline = timeline;
-      return timeline;
-    }
-
-    centerArrows() {
-      if (!this.prevButton || !this.nextButton) return;
-
-      const firstCell = this.flickity.cells[0];
-      const image = firstCell.element.querySelector('.product-featured-image');
-      const imageHeight = image ? image.clientHeight : 0;
-
-      const maxHeight = Math.max(...this.flickity.cells.map(cell => cell.size.height));
-
-      if (maxHeight > imageHeight) {
-        const difference = (maxHeight - imageHeight) / -2;
-        const transform = `translateY(${difference}px)`;
-        this.prevButton.style.transform = transform;
-        this.nextButton.style.transform = transform;
-      } else {
-        this.prevButton.style.transform = '';
-        this.nextButton.style.transform = '';
+        if (prev_button) {
+          prev_button.style.transform = 'translateY(' + difference + 'px)';
+        }
+        if (next_button) {
+          next_button.style.transform = 'translateY(' + difference + 'px)';
+        }
       }
     }
   }
-
   customElements.define('slide-show', SlideShow);
 }
